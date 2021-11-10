989b1291d5b2a183de351d42f597e21bbb27e1f9
BBeta-Cam <91014480+BBeta-Cam@users.noreply.github.com>
2021-11-10 17:14:56 +0200

    added comments

--- GADE6122_POE_CameronReeceHanger_20125973/Form1.cs
+++ GADE6122_POE_CameronReeceHanger_20125973/Form1.cs
@@ -25,28 +25,28 @@ namespace GADE6122_POE_CameronReeceHanger_20125973
 
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog open = new OpenFileDialog();
-            open.Title = "OPen File";
+            OpenFileDialog open = new OpenFileDialog();//creates new object
+            open.Title = "Open File"; //renames title
 
             if (open.ShowDialog() == DialogResult.OK)
             {
-                StreamReader read = new StreamReader(File.OpenRead(open.FileName));
+                StreamReader read = new StreamReader(File.OpenRead(open.FileName)); //reads file
 
-                textBox1.Text = read.ReadToEnd();
-                read.Dispose();
+                textBox1.Text = read.ReadToEnd();//reads the whole file
+                read.Dispose();//closes file to avoid reopening errors
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            SaveFileDialog save = new SaveFileDialog();
+            SaveFileDialog save = new SaveFileDialog(); //new object
 
             if(save.ShowDialog() == DialogResult.OK)
             {
-                StreamWriter write = new StreamWriter(File.Create(save.FileName));
+                StreamWriter write = new StreamWriter(File.Create(save.FileName)); 
 
-                write.Write(textBox1.Text);
-                write.Dispose();
+                write.Write(textBox1.Text);//names new file
+                write.Dispose();//closes file to avoid reopening errors
 
             }
         }
