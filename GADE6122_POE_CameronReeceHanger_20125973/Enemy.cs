f91872e0fca70cc0f9133eb84f3b86ceeec49df8
BBeta-Cam <91014480+BBeta-Cam@users.noreply.github.com>
2021-11-10 17:10:33 +0200

    Added comments

--- GADE6122_POE_CameronReeceHanger_20125973/Enemy.cs
+++ GADE6122_POE_CameronReeceHanger_20125973/Enemy.cs
@@ -6,24 +6,24 @@ using System.Threading.Tasks;
 
 namespace GADE6122_POE_CameronReeceHanger_20125973
 {
-    public abstract class Enemy : Character
+    public abstract class Enemy : Character //enemy inherits from character
     {
-        protected Random RandomNum = new Random();
+        protected Random RandomNum = new Random(); // variable that creates random number
 
-        protected Enemy(int _X, int _Y, int _DAMAGE, int _STARTINGHP) : base(_X,_Y,_DAMAGE,_STARTINGHP)
+        protected Enemy(int _X, int _Y, int _DAMAGE, int _STARTINGHP) : base(_X,_Y,_DAMAGE,_STARTINGHP) //constructor
         {
             DAMAGE = _DAMAGE;
             HP = _STARTINGHP;
 
         }
-
-        public override string ToString()
+ 
+        public override string ToString()//ToString concatinates name, x and y coordinates and damage into one single string
         {
             string EnemyInfo = GetType().Name + " " + "at" + " ";
             EnemyInfo += "[" + X.ToString() + "," + Y.ToString() + "]" + " ";
             EnemyInfo += "(" + DAMAGE.ToString() + ")";
 
-            return EnemyInfo;
+            return EnemyInfo; //returns final string
         }
     }
 }
