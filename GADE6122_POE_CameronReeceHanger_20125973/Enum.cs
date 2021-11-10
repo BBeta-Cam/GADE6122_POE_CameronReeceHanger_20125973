6a9558bda42855d64b2c4f931e50bd75c6cc264b
BBeta-Cam <91014480+BBeta-Cam@users.noreply.github.com>
2021-11-10 17:12:01 +0200

    added comments

--- GADE6122_POE_CameronReeceHanger_20125973/Enum.cs
+++ GADE6122_POE_CameronReeceHanger_20125973/Enum.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace GADE6122_POE_CameronReeceHanger_20125973
 {
     
-        public enum TileType 
+        public enum TileType //declaring values that will hold parameters
         { 
         Hero, 
         Enemy,
         Gold,
         }
 
-        public enum MovementEnum
+        public enum MovementEnum //these values dictate the types of movement the players and enemies can have
         {
             NoMovement,
             Up,
